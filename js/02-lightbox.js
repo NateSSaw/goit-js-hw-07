b1e86e5a95072ee8649aa4533219a96d68e99911
Nataliia Semenova <natasemenova1806@gmail.com>
2023-01-09 17:30:01 +0200

    done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+const cardsMarkup = createCards(galleryItems);
+
+function createCards(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+const gallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
